7a05ba9e6c90fd94b7837062d07b8e335a7c8aec
sczsh <sczsh@posteo.de>
2017-01-21 21:54:40 +0100

    Moved set up for the vennmaker workspace to own util class // further cleanup

--- src/main/java/gui/VennMaker.java
+++ src/main/java/gui/VennMaker.java
@@ -45,6 +45,7 @@ import org.jvnet.flamingo.common.icon.ResizableIcon;
 import org.jvnet.flamingo.ribbon.JRibbon;
 import org.jvnet.flamingo.ribbon.RibbonTask;
 import org.jvnet.flamingo.svg.SvgBatikResizableIcon;
+import util.Environment;
 import wizards.WizardController;
 
 import javax.swing.*;
@@ -52,18 +53,11 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.awt.dnd.*;
 import java.awt.event.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.List;
 
@@ -250,28 +244,15 @@ public class VennMaker extends JFrame {
 
     private String exportDefaultPath = "";
 
-    private static ArrayList<IModule> module = new ArrayList<IModule>();
+    private static List<IModule> module = new ArrayList<>();
 
     /**
      * Get module
      *
      * @return IModule
      */
-    public ArrayList<IModule> getModule() {
+    public List<IModule> getModule() {
         return this.module;
-    }
-
-    /**
-     * Set plugin modules
-     *
-     * @param module
-     */
-    public static void setModule(IModule m) {
-        System.out.println("---------------------");
-        System.out.println("Modul: " + m.getModuleName());
-        System.out.println("Version: " + m.getVersion());
-
-        module.add(m);
     }
 
     public static VennMaker getInstance() {
@@ -2041,61 +2022,6 @@ public class VennMaker extends JFrame {
     }
 
     /**
-     * Alle Plugins im Verzeichnis Module laden
-     */
-    private static void loadPlugins() {
-
-        String currentRelativePath = Paths.get("").toAbsolutePath().toString();
-
-        String modulPath = currentRelativePath + "/module/";
-
-        String[] entries = new File(modulPath).list();
-
-        if (entries != null)
-            for (String moduleDateiName : entries) {
-                if (moduleDateiName.endsWith(".jar")) {
-
-                    File f = new File(modulPath + moduleDateiName);
-
-                    String modulName = moduleDateiName.substring(0,
-                            moduleDateiName.indexOf(".jar"));
-
-                    URLClassLoader classLoader;
-                    try {
-                        classLoader = new URLClassLoader(
-                                new URL[]{f.toURI().toURL()},
-                                ClassLoader.getSystemClassLoader());
-                        Class moduleC;
-                        try {
-                            /**
-                             * Namenskonvention: Der Packetname ist gleich dem Modulname
-                             * (z.B. vennmakerhist = vennmakerhist.jar) Main Klasse heisst
-                             * immer Main (Achtung: grosses M)
-                             */
-                            moduleC = classLoader.loadClass("de.vennmakermodule."
-                                    + modulName + ".Main");
-                            setModule((IModule) moduleC.newInstance());
-
-                        } catch (ClassNotFoundException e) {
-
-                            System.out.println("Module: " + e);
-                        } catch (InstantiationException e) {
-                            System.out.println("Module: " + e);
-                        } catch (IllegalAccessException e) {
-                            System.out.println("Module: " + e);
-                        }
-                    } catch (MalformedURLException e1) {
-                        // TODO Auto-generated catch block
-                        e1.printStackTrace();
-                    }
-
-                }
-            }
-
-    }
-
-
-    /**
      * Load and set the PluginModule configuration from the vmp file
      */
     private static void loadModuleData() {
@@ -2134,16 +2060,8 @@ public class VennMaker extends JFrame {
             startMode = showStartChooserAndGetStartMode();
         }
 
-        Thread.setDefaultUncaughtExceptionHandler(new ErrorUncaughtExceptionHandler());
-
-        Locale.setDefault(Locale.ENGLISH);
-
-        VennMaker.getInstance().createConfigDialogLayer();
-        validateIconFolders();
-        createVennMakerFolders();
-        loadPlugins();
-        setUIManager();
-
+        Environment.initializeWorkspace();
+        module = Environment.loadPlugins();
         startVennMakerInSelectedMode(startMode);
     }
 
@@ -2197,36 +2115,6 @@ public class VennMaker extends JFrame {
 
             if (diag.showLoadTemplateDialog(false) == IO.OPERATION_SUCCEEDED)
                 diag.setVisible(true);
-        }
-    }
-
-    private static void validateIconFolders() {
-        String missingFolders = FileOperations.getMissingFolders();
-        if (!missingFolders.equals("")) {
-            JOptionPane.showMessageDialog(
-                    null,
-                    Messages.getString("VennMaker.MissingFolders.1")
-                            + missingFolders
-                            + Messages.getString("VennMaker.MissingFolders.2"),
-                    Messages.getString("VennMaker.MissingFoldersTitle"),
-                    JOptionPane.ERROR_MESSAGE);
-            VennMaker.exit();
-        }
-    }
-
-    private static void createVennMakerFolders() {
-        File folder = new File(VMPaths.getCurrentWorkingRoot()); //$NON-NLS-1$ //$NON-NLS-2$
-        folder.mkdirs();
-        File tempProjects = new File(VMPaths.getCurrentWorkingDirectory());
-        tempProjects.mkdir();
-        FileOperations.createSubfolders(tempProjects.getAbsolutePath());
-    }
-
-    private static void setUIManager() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException exn) {
-            exn.printStackTrace();
         }
     }
 
@@ -2827,361 +2715,5 @@ public class VennMaker extends JFrame {
             }
         }
         return true;
-    }
-}
-
-// Code aus dem Internet
-class DDTabPane extends JTabbedPane {
-    private static final long serialVersionUID = 1L;
-
-    private final GhostGlassPane glassPane = new GhostGlassPane();
-
-    private final Rectangle lineRect = new Rectangle();
-
-    private final Color lineColor = Color.blue;
-
-    private int dragTabIndex = -1;
-
-    private static final int LINEWIDTH = 6;
-
-    private static final String NAME = "test";                    //$NON-NLS-1$
-
-    private Point tabPt;
-
-    public DDTabPane() {
-        super();
-        final DragSourceListener dsl = new DragSourceListener() {
-            public void dragEnter(DragSourceDragEvent e) {
-                e.getDragSourceContext().setCursor(DragSource.DefaultMoveDrop);
-            }
-
-            public void dragExit(DragSourceEvent e) {
-                e.getDragSourceContext().setCursor(DragSource.DefaultMoveNoDrop);
-                lineRect.setRect(0, 0, 0, 0);
-                glassPane.setPoint(new Point(-1000, -1000));
-                glassPane.repaint();
-            }
-
-            public void dragOver(DragSourceDragEvent e) {
-                Point glassPt = e.getLocation();
-                SwingUtilities.convertPointFromScreen(glassPt, glassPane);
-                int targetIdx = getTargetTabIndex(glassPt);
-                if (getTabAreaBounds().contains(glassPt) && targetIdx >= 0
-                        && targetIdx != dragTabIndex && targetIdx != dragTabIndex + 1) {
-                    e.getDragSourceContext().setCursor(DragSource.DefaultMoveDrop);
-                } else {
-                    e.getDragSourceContext().setCursor(DragSource.DefaultMoveNoDrop);
-                }
-            }
-
-            public void dragDropEnd(DragSourceDropEvent e) {
-                lineRect.setRect(0, 0, 0, 0);
-                dragTabIndex = -1;
-                glassPane.setVisible(false);
-                if (hasGhost()) {
-                    glassPane.setVisible(false);
-                    glassPane.setImage(null);
-                }
-            }
-
-            public void dropActionChanged(DragSourceDragEvent e) {
-            }
-        };
-        final Transferable t = new Transferable() {
-            private final DataFlavor FLAVOR = new DataFlavor(
-                    DataFlavor.javaJVMLocalObjectMimeType,
-                    NAME);
-
-            public Object getTransferData(DataFlavor flavor) {
-                return DDTabPane.this;
-            }
-
-            public DataFlavor[] getTransferDataFlavors() {
-                DataFlavor[] f = new DataFlavor[1];
-                f[0] = this.FLAVOR;
-                return f;
-            }
-
-            public boolean isDataFlavorSupported(DataFlavor flavor) {
-                return flavor.getHumanPresentableName().equals(NAME);
-            }
-        };
-        final DragGestureListener dgl = new DragGestureListener() {
-            public void dragGestureRecognized(DragGestureEvent e) {
-                if (getTabCount() <= 1)
-                    return;
-                Point tabPt = e.getDragOrigin();
-                dragTabIndex = indexAtLocation(tabPt.x, tabPt.y);
-                if (dragTabIndex < 0 || !isEnabledAt(dragTabIndex))
-                    return;
-                initGlassPane(e.getComponent(), e.getDragOrigin());
-                try {
-                    e.startDrag(DragSource.DefaultMoveDrop, t, dsl);
-                } catch (InvalidDnDOperationException idoe) {
-                    idoe.printStackTrace();
-                }
-            }
-        };
-        new DropTarget(glassPane, DnDConstants.ACTION_COPY_OR_MOVE,
-                new CDropTargetListener(), true);
-        new DragSource().createDefaultDragGestureRecognizer(this,
-                DnDConstants.ACTION_COPY_OR_MOVE, dgl);
-    }
-
-    class CDropTargetListener implements DropTargetListener {
-        public void dragEnter(DropTargetDragEvent e) {
-            if (isDragAcceptable(e))
-                e.acceptDrag(e.getDropAction());
-            else
-                e.rejectDrag();
-        }
-
-        public void dragExit(DropTargetEvent e) {
-        }
-
-        public void dropActionChanged(DropTargetDragEvent e) {
-        }
-
-        private Point pt_ = new Point();
-
-        public void dragOver(final DropTargetDragEvent e) {
-            Point pt = e.getLocation();
-            if (getTabPlacement() == JTabbedPane.TOP
-                    || getTabPlacement() == JTabbedPane.BOTTOM) {
-                initTargetLeftRightLine(getTargetTabIndex(pt));
-            } else {
-                initTargetTopBottomLine(getTargetTabIndex(pt));
-            }
-            if (hasGhost()) {
-                glassPane.setPoint(pt);
-            }
-            if (!pt_.equals(pt))
-                glassPane.repaint();
-            pt_ = pt;
-        }
-
-        public void drop(DropTargetDropEvent e) {
-            if (isDropAcceptable(e)) {
-                convertTab(dragTabIndex, getTargetTabIndex(e.getLocation()));
-                e.dropComplete(true);
-            } else {
-                e.dropComplete(false);
-            }
-            repaint();
-        }
-
-        public boolean isDragAcceptable(DropTargetDragEvent e) {
-            Transferable t = e.getTransferable();
-            if (t == null)
-                return false;
-            DataFlavor[] f = e.getCurrentDataFlavors();
-            if (t.isDataFlavorSupported(f[0]) && dragTabIndex >= 0) {
-                return true;
-            }
-            return false;
-        }
-
-        public boolean isDropAcceptable(DropTargetDropEvent e) {
-            Transferable t = e.getTransferable();
-            if (t == null)
-                return false;
-            DataFlavor[] f = t.getTransferDataFlavors();
-            if (t.isDataFlavorSupported(f[0]) && dragTabIndex >= 0) {
-                return true;
-            }
-            return false;
-        }
-    }
-
-    private boolean hasGhost = true;
-
-    public void setPaintGhost(boolean flag) {
-        hasGhost = flag;
-    }
-
-    public boolean hasGhost() {
-        return hasGhost;
-    }
-
-    private int getTargetTabIndex(Point glassPt) {
-        Point tabPt = SwingUtilities.convertPoint(glassPane, glassPt,
-                DDTabPane.this);
-        boolean isTB = getTabPlacement() == JTabbedPane.TOP
-                || getTabPlacement() == JTabbedPane.BOTTOM;
-        for (int i = 0; i < getTabCount(); i++) {
-            Rectangle r = getBoundsAt(i);
-            if (isTB)
-                r.setRect(r.x - r.width / 2, r.y, r.width, r.height);
-            else
-                r.setRect(r.x, r.y - r.height / 2, r.width, r.height);
-            if (r.contains(tabPt))
-                return i;
-        }
-        Rectangle r = getBoundsAt(getTabCount() - 1);
-        if (isTB)
-            r.setRect(r.x + r.width / 2, r.y, r.width, r.height);
-        else
-            r.setRect(r.x, r.y + r.height / 2, r.width, r.height);
-        return r.contains(tabPt) ? getTabCount() : -1;
-    }
-
-    @Override
-    public void addTab(String name, Component comp) {
-        super.addTab(name, null, comp, name);
-    }
-
-    @Override
-    public String getTitleAt(int i) {
-        String str = super.getTitleAt(i);
-        if (str.length() > 13)
-            str = str.substring(0, 10) + "...";
-
-        return str;
-    }
-
-    @Override
-    public String getToolTipTextAt(int i) {
-        return super.getTitleAt(i);
-    }
-
-    private void convertTab(int prev, int next) {
-        if (next < 0 || prev == next) {
-            return;
-        }
-        Component cmp = getComponentAt(prev);
-        Component tab = getTabComponentAt(prev);
-        String str = getTitleAt(prev);
-        Icon icon = getIconAt(prev);
-        String tip = getToolTipTextAt(prev);
-        boolean flg = isEnabledAt(prev);
-        int tgtindex = prev > next ? next : next - 1;
-        remove(prev);
-        insertTab(str, icon, cmp, tip, tgtindex);
-        Netzwerk net = VennMaker.getInstance().getProject().getNetzwerke()
-                .get(prev);
-        VennMaker.getInstance().getProject().getNetzwerke().remove(net);
-        VennMaker.getInstance().getProject().getNetzwerke()
-                .insertElementAt(net, tgtindex);
-        setEnabledAt(tgtindex, flg);
-        if (flg)
-            setSelectedIndex(tgtindex);
-        setTabComponentAt(tgtindex, tab);
-    }
-
-    private void initTargetLeftRightLine(int next) {
-        if (next < 0 || dragTabIndex == next || next - dragTabIndex == 1) {
-            lineRect.setRect(0, 0, 0, 0);
-        } else if (next == 0) {
-            Rectangle r = SwingUtilities.convertRectangle(this, getBoundsAt(0),
-                    glassPane);
-            lineRect.setRect(r.x - LINEWIDTH / 2, r.y, LINEWIDTH, r.height);
-        } else {
-            Rectangle r = SwingUtilities.convertRectangle(this,
-                    getBoundsAt(next - 1), glassPane);
-            lineRect.setRect(r.x + r.width - LINEWIDTH / 2, r.y, LINEWIDTH,
-                    r.height);
-        }
-    }
-
-    private void initTargetTopBottomLine(int next) {
-        if (next < 0 || dragTabIndex == next || next - dragTabIndex == 1) {
-            lineRect.setRect(0, 0, 0, 0);
-        } else if (next == 0) {
-            Rectangle r = SwingUtilities.convertRectangle(this, getBoundsAt(0),
-                    glassPane);
-            lineRect.setRect(r.x, r.y - LINEWIDTH / 2, r.width, LINEWIDTH);
-        } else {
-            Rectangle r = SwingUtilities.convertRectangle(this,
-                    getBoundsAt(next - 1), glassPane);
-            lineRect.setRect(r.x, r.y + r.height - LINEWIDTH / 2, r.width,
-                    LINEWIDTH);
-        }
-    }
-
-    private void initGlassPane(Component c, Point tabPt) {
-        getRootPane().setGlassPane(glassPane);
-        if (hasGhost()) {
-            Rectangle rect = getBoundsAt(dragTabIndex);
-            BufferedImage image = new BufferedImage(c.getWidth(), c.getHeight(),
-                    BufferedImage.TYPE_INT_ARGB);
-            Graphics g = image.getGraphics();
-            c.paint(g);
-            rect.x = rect.x < 0 ? 0 : rect.x;
-            rect.y = rect.y < 0 ? 0 : rect.y;
-
-            // Ueberpruefen ob Tab nicht zu gross (durch Ueberlaenge beim Netzwerk
-            // Namen
-            // z.B.)
-            rect.width = rect.width >= image.getWidth() ? image.getWidth()
-                    - rect.x - 35 : rect.width;
-
-            image = image.getSubimage(rect.x, rect.y, rect.width, rect.height);
-            glassPane.setImage(image);
-        }
-        Point glassPt = SwingUtilities.convertPoint(c, tabPt, glassPane);
-        this.tabPt = tabPt;
-        glassPane.setPoint(glassPt);
-        glassPane.setVisible(true);
-    }
-
-    private Rectangle getTabAreaBounds() {
-        Rectangle tabbedRect = getBounds();
-        Component comp = getSelectedComponent();
-        int idx = 0;
-        while (comp == null && idx < getTabCount())
-            comp = getComponentAt(idx++);
-        Rectangle compRect = (comp == null) ? new Rectangle() : comp.getBounds();
-        int tabPlacement = getTabPlacement();
-        if (tabPlacement == TOP) {
-            tabbedRect.height = tabbedRect.height - compRect.height;
-        } else if (tabPlacement == BOTTOM) {
-            tabbedRect.y = tabbedRect.y + compRect.y + compRect.height;
-            tabbedRect.height = tabbedRect.height - compRect.height;
-        } else if (tabPlacement == LEFT) {
-            tabbedRect.width = tabbedRect.width - compRect.width;
-        } else if (tabPlacement == RIGHT) {
-            tabbedRect.x = tabbedRect.x + compRect.x + compRect.width;
-            tabbedRect.width = tabbedRect.width - compRect.width;
-        }
-        tabbedRect.grow(2, 2);
-        return tabbedRect;
-    }
-
-    class GhostGlassPane extends JPanel {
-        private static final long serialVersionUID = 1L;
-
-        private final AlphaComposite composite;
-
-        private Point location = new Point(0, 0);
-
-        private BufferedImage draggingGhost = null;
-
-        public GhostGlassPane() {
-            setOpaque(false);
-            composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f);
-            setCursor(null); // xxx
-        }
-
-        public void setImage(BufferedImage draggingGhost) {
-            this.draggingGhost = draggingGhost;
-        }
-
-        public void setPoint(Point location) {
-            this.location = location;
-        }
-
-        public void paintComponent(Graphics g) {
-            Graphics2D g2 = (Graphics2D) g;
-            g2.setComposite(composite);
-            if (draggingGhost != null) {
-                double xx = location.getX() - (draggingGhost.getWidth(this) / 2d);
-                double yy = location.getY() - (draggingGhost.getHeight(this) / 2d);
-                g2.drawImage(draggingGhost, (int) xx, (int) yy, null);
-            }
-            if (dragTabIndex >= 0) {
-                g2.setPaint(lineColor);
-                g2.fill(lineRect);
-            }
-        }
     }
 }
